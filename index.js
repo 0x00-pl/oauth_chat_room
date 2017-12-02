19e7f9c471a5fece432ac96f2b3a58e1ffbe565a
0x00-pl <0x00.pl@gmail.com>
2017-12-02 23:01:30 +0800

    fix bugs

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function login () {
     window.location = window.config.api_server+'/oauth0?cb='+cb
 }
 function logout() {
-    window.localStroage.deleteItem('token')
+    window.localStorage.removeItem('token')
     window.location.reload()
 }
 function api_get_global_token(){
@@ -23,8 +23,12 @@ function get_username(){
     let token = api_get_global_token()
     fetch(config.api_server+'/api/user?token='+token).then(b=>b.json())
 	.then(a=>{
+	    if(a.login){
 	    console.log('[debug] user:', a)
-	    console.log('[debug] username:', a.login)
+		console.log('[debug] username:', a.login)
+	    } else {
+		logout()
+	    }
 	})
 }
 
